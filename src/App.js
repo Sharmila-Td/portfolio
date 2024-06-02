8b691d54baf6141a4c0d1e4f1560a19a510f4297
sharmila-developer <sharmi.webdev@gmail.com>
2024-06-02 09:37:20 -0700

    created a project folder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Main from './Components/Main';
 import Navbar from './Components/Navbar';
+import Project from './Components/Project';
 
 function App() {
   return (
     <div className="container">
           <Navbar/>
           <Main/>
+          <Project/>
     </div>
   );
 }
