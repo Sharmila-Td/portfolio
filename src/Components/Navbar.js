89aa0cf4f2146f30cc7323cfd27aebfeb0e9fa7a
sharmila-developer <sharmi.webdev@gmail.com>
2024-06-04 18:06:17 -0700

    adding responsive to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
+import { FiAlignJustify } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
 const Navbar = () => {
+  const menubar = useRef();
+
+  const openMenu = () => {
+    menubar.current.style.left = '0'
+  }
+  const closeMenu = () => {
+    menubar.current.style.left = "-350px"
+  }
   return (
     <>
       <div className="nav">
-        <h3>
-          <i>Sha</i>
-        </h3>
-        <ul className="nav-list">
+        <h3><i>Sha</i></h3>
+        <pa className='nav-open' onClick={openMenu} > <FiAlignJustify/> </pa>
+        <ul className="nav-list" ref={menubar}>
+          <p className="nav-close" onClick={closeMenu}> <FiX/> </p>
           <li>Home</li>
           <li>About</li>
           <li>Project</li>
